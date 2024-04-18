b74ae577515c22644eff3265004ebb166045610f
WillP2026 <157647180+WillP2026@users.noreply.github.com>
2024-04-18 14:52:49 -0400

    Replaced SQL values with session variable

--- form.php
+++ form.php
@@ -32,7 +32,7 @@
   <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <div class="container">
-      <h1 class="text-center">Hello <?php echo $row['firstname']. ' ' . $row['lastname'];  ?>  </h1>
+      <h1 class="text-center">Hello <?php echo "Hello {$_SESSION['firstname']} {$_SESSION['lastname']}";  ?>  </h1>
       <div class="realform"> 
         <form>
           <div class="row"> 
